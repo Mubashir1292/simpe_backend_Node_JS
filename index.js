1a00f177f0fe4597e46bcaf339c426211e04698a
Mubashir1292 <mubashirliaqat72@gmail.com>
2025-05-02 07:51:01 +0500

    add some routes and form encoded url as well..

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const Product = require("./models/product.model.js");
 const app = express();
 // ! Adding Middle ware
 app.use(express.json());
+//! Adding form Encoded
+app.use(express.urlencoded({extended:false}));
 //! First connection with database
 mongoose.connect(`mongodb+srv://mubashirliaqat72:adminPassword@samplecluster.hpg7p42.mongodb.net/SAMPLE-NODE-BACKEND-APP?retryWrites=true&w=majority&appName=sampleCluster`)
     .then(() => {
@@ -24,6 +26,16 @@ app.get('/api/products', async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
+// ! finding the single product
+app.get('/api/product/:id',async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+})
 app.post('/api/createProduct', async (req, res) => {
     try {
         const newProduct = await Product.create(req.body);
@@ -32,4 +44,31 @@ app.post('/api/createProduct', async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 })
+//? update the product api...
+app.put('/api/updateProduct/:id',async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const product = await Product.findByIdAndUpdate(id,req.body);
+        if(!product){
+            res.status(404).json({message:'Product not founded'});
+        }
+        res.status(200).json(product)
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+})
+//? delete the product api...
+app.delete("/api/product/:id",async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            res.status(404).json({message:'Product Not founded'});
+        }
+        res.status(200).json({message:'Product Deleted Successfully..'});
 
+
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+})
